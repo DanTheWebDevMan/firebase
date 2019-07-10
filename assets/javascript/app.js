5b1e8eac695524e3adb0d78f0f87db79e3f47c1d
Danniel Zelaya <dannielzelaya@gmail.com>
2019-07-10 18:37:49 -0500

    added correct jquery library

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,7 +7,7 @@
 */
 // Your web app's Firebase configuration
  
-$(document).ready(function(){
+// $(document).ready(function(){
 
     //code from firebase
     var firebaseConfig = {
@@ -15,13 +15,15 @@ $(document).ready(function(){
         authDomain: "fir-hw-e5ffa.firebaseapp.com",
         databaseURL: "https://fir-hw-e5ffa.firebaseio.com",
         projectId: "fir-hw-e5ffa",
-        storageBucket: "",
+        storageBucket: "Firebase HW",
         messagingSenderId: "763004003824",
         appId: "1:763004003824:web:51f742b02e3ac99c"
       };
       // Initialize Firebase
       firebase.initializeApp(firebaseConfig);
-      var database = firebase.database();
+
+      var database = firebase.storage();
+        
   
       // Sample code of how to add a new Table Row
       $("#submit").click(function(){
@@ -43,27 +45,27 @@ $(document).ready(function(){
           console.log("Adding new row!");
           
           
-          var newRow = "<tr><td>" + 
-              nameInput + "</td><td>" + 
-              roleInput + "</td><td>" + 
-              startInput + "</td><td>" + 
-              monthsWorked + "</td><td>" + 
-              rateInput + "</td><td>";
+          var newRow = "<tr>" + 
+              nameInput + "<td>" + 
+              roleInput + "<td>" + 
+              startInput + "<td>" + 
+              monthsWorked + "<td>" + 
+              rateInput + "<td>";
              
-          $("#tbody").append(newRow); 
+          $("#contentox").append(newRow); 
       });
     
       database.ref().on("value", function(snapshot){
         var newRow = $("<th>");
-        // snapshot.val().array.forEach(element => {
-        //   console.log(element.name);
-        // });
+        snapshot.val().array.forEach(element => {
+          console.log(element.name);
+        });
         Object.keys(obj).forEach(function(key) {
           console.log(key, obj[key]);
         })
   
         var currentName = $("<td>");
-        //currentName.text(database);
+        currentName.text(database);
        // console.log(snapshot.val(name));
         var currentDestination = $("<td>");
         var currentTrainTime = $("<td>");
@@ -73,8 +75,8 @@ $(document).ready(function(){
         console.log(snapshot.val());
   
         
+      });
   
-    }) 
   
-  })
-  
+  // })
+  //define firebase storage bucket
