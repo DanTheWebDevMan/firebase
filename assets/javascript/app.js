dd33543b0ffbff012d3077b0bb6afb14d10cef75
Danniel Zelaya <dannielzelaya@gmail.com>
2019-07-11 21:35:06 -0500

    data put into form displaying in firebase database but not in created table

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,68 +15,195 @@
         authDomain: "fir-hw-e5ffa.firebaseapp.com",
         databaseURL: "https://fir-hw-e5ffa.firebaseio.com",
         projectId: "fir-hw-e5ffa",
-        storageBucket: "Firebase HW",
+        storageBucket: "",
         messagingSenderId: "763004003824",
         appId: "1:763004003824:web:51f742b02e3ac99c"
       };
+
       // Initialize Firebase
       firebase.initializeApp(firebaseConfig);
 
-      var database = firebase.storage();
-        
-  
+
+      //create database variable to create to firebase.database()
+      var database = firebase.database();
+
       // Sample code of how to add a new Table Row
-      $("#submit").click(function(){
-          var nameInput = $("#nameInput").val();
-          var destInput = $("#destInput").val();
-          var timeInput = $("#timeInput").val();
-          var freqInput = $("#freqInput").val();
-         // var monthsWorked = firebase.database.ServerValue.TIMESTAMP;
-        
-        // code for handling the push
-        database.ref().push({
-          trainName: nameInput,
-          destInput: destInput,
-          timeInput: timeInput,
-          freqInput: freqInput,
-          //monthsWorked: monthsWorked,
-        })
+      $("#submit").on("click", function(event){
+        event.preventDefault();
+
+        var nameInput = $("#nameInput").val().trim();
+        var destInput = $("#destInput").val().trim();
+        var firstTrainInput = $("#firstTrainInput").val().trim();
+        var freqInput = $("#freqInput").val().trim();
+       // var monthsWorked = firebase.database.ServerValue.TIMESTAMP;
+      //  if (nameInput === "") {
+      //   alert("Please enter in a train name.");
+      //   return false;
+      // }
+
+      // if (destInput === "") {
+      //     alert("Please enter in a destination.");
+      //     return false;
+      //   }
   
-          console.log("Adding new row!");
+      // if (firstTrainInput === "") {
+      //     alert("Please enter in the first train time.");
+      //     return false;
+      //   }
+      // //need to make an if statement to ensure that the time entered is correct
+      // if (freqInput === "") {
+      //     alert("Please enter in a frequency.");
+      //     return false;
+      //   }
+
+    //Stores it in a JSON object called newTrain
+    var newTrain = {
+        nameInput: nameInput,
+        destInput: destInput,
+        firsTrainInput: firstTrainInput,
+        freqInput: freqInput
+    };
+
+    database.ref().push(newTrain);
+    console.log(newTrain);
+
+    alert("New train added!");
+
+       //Clears the form
+    $("#trainNameInput").val("");
+    $("#destinationInput").val("");
+    $("#destinationInput").val("");
+    $("#frequencyInput").val("");
+
+  });
+
+  //Firebase event
+    database.ref().on("child_added", function(childSnapshot) {
+
+    //Storing the firebase snapshot into a variable
+    var nameInput = childSnapshot.val().nameInput;
+    var destInput = childSnapshot.val().destInput;
+    var firstTrainInput = childSnapshot.val().firsTrainInput;
+    var freqInput = childSnapshot.val().freqInput;
           
+    // var tMinutesTillTrain = 0;
+
+      // //show an update current time. setInterval method udpates time
+      // function displayRealtime() {
+      //   setInterval(function () {
+      //     $('#current-time').html(moment().format('hh:mm A'))
+      //     }, 1000);
+      //   } 
+
+      //   // displayRealTime();
+
+      // var tRow = "";
+      // var getKey = "";
+
+      //   //Click event for the submit button. When user clicks Submit button to add a train to the schedule...
+      // $("#submit").on("click", function() {
+	    //     // Prevent form from submitting
+      //     event.preventDefault();
+
+      //     //Grab the values that the user enters in the text boxes in the "Add train" section. Store the values in variables.
+      //     var nameInput = $("#nameInput").val().trim();
+      //     var destInput = $("#destInput").val().trim();
+      //     var firstTrainInput = $("#firsTrainInput").val().trim();
+      //     var freqInput = $("#freqInput").val().trim();
+      //     //Print the values that the user enters in the text boxes to the console for debugging purposes.
+      //       console.log(nameInput);
+      //       console.log(destInput);
+      //       console.log(firstTrainInput);
+      //       console.log(freqInput);
+
+      //     //Form validation for user input values. To add a train, all fields are required.
+	    //     //Check that all fields are filled out.
+	    //     if (nameInput === "" || destInput === "" || firstTrainInput === "" || freqInput === ""){
+	  	//     $("#firsTrainInput").empty();
+		  //     $("#missing-field").html("ALL fields are required to add a train to the schedule.");
+		  // return false;		
+	    // }
+      //     })
+
+      //     //If form is valid, perform time calculations and add train to the current schedule.
+      // else {
+      //   $("#not-military-time").empty();
+      //   $("#missing-field").empty();
+      //   $("#not-a-number").empty();
           
-          var newRow = "<tr>" + 
-              nameInput + "<td>" + 
-              roleInput + "<td>" + 
-              startInput + "<td>" + 
-              monthsWorked + "<td>" + 
-              rateInput + "<td>";
-             
-          $("#contentox").append(newRow); 
-      });
+    // //current time
+    // var currentTime = moment();
+
+    // //This subtracts the first train by a year to make sure it's before the current time (moment)
+    // var firstTrainConverted = moment(firstTrainInput, "hh:mm").subtract("1, years");
+    // // This calculates the difference between the current time and the first train
+    // var difference = currentTime.diff(moment(firstTrainConverted), "minutes");
+    // var remainder = difference % frequency;
+    // var minutesAway = frequency - remainder;
+    // var nextArrival = moment().add(minutesAway, "minutes").format("HH:mm");
+
+    //create the new rows and data cells
+    var newRow = $("<tr>").append(
+        $("<td>").text(nameInput),
+        $("<td>").text(destInput),
+        $("<td>").text(freqInput),
+        // $("<td>").text(nextArrival),
+      //   $("<td>").text(minutesAway),
+      // );
+
+      //append to the table
+      $("#trainTable > tbody").append(newRow));
+
+    });
     
-      database.ref().on("value", function(snapshot){
-        var newRow = $("<th>");
-        snapshot.val().array.forEach(element => {
-          console.log(element.name);
-        });
-        Object.keys(obj).forEach(function(key) {
-          console.log(key, obj[key]);
-        })
-  
-        var currentName = $("<td>");
-        currentName.text(database);
-       // console.log(snapshot.val(name));
-        var currentDestination = $("<td>");
-        var currentTrainTime = $("<td>");
-        var currentFrequency = $("<td>");
-        newRow.append(currentName, currentDestination, currentTrainTime, currentFrequency);
-        var currentName = $("<td>");
-        console.log(snapshot.val());
-  
+
+//define firebase storage bucket
+
+      // var tMinutesTillTrain = 0;
+
+      // //show an update current time. setInterval method udpates time
+      // function displayRealtime() {
+      //   setInterval(function () {
+      //     $('#current-time').html(moment().format('hh:mm A'))
+      //     }, 1000);
+      //   } 
+
+      //   // displayRealTime();
+
+      // var tRow = "";
+      // var getKey = "";
+
+      //   //Click event for the submit button. When user clicks Submit button to add a train to the schedule...
+      // $("#submit").on("click", function() {
+	    //     // Prevent form from submitting
+      //     event.preventDefault();
+
+      //     //Grab the values that the user enters in the text boxes in the "Add train" section. Store the values in variables.
+      //     var nameInput = $("#nameInput").val().trim();
+      //     var destInput = $("#destInput").val().trim();
+      //     var firstTrainInput = $("#firsTrainInput").val().trim();
+      //     var freqInput = $("#freqInput").val().trim();
+      //     //Print the values that the user enters in the text boxes to the console for debugging purposes.
+      //       console.log(nameInput);
+      //       console.log(destInput);
+      //       console.log(firstTrainInput);
+      //       console.log(freqInput);
+
+      //     //Form validation for user input values. To add a train, all fields are required.
+	    //     //Check that all fields are filled out.
+	    //     if (nameInput === "" || destInput === "" || firstTrainInput === "" || freqInput === ""){
+	  	//     $("#firsTrainInput").empty();
+		  //     $("#missing-field").html("ALL fields are required to add a train to the schedule.");
+		  // return false;		
+	    // }
+      //     })
+
+      //     //If form is valid, perform time calculations and add train to the current schedule.
+      // else {
+      //   $("#not-military-time").empty();
+      //   $("#missing-field").empty();
+      //   $("#not-a-number").empty();
+          
         
-      });
   
-  
-  // })
-  //define firebase storage bucket
+      
